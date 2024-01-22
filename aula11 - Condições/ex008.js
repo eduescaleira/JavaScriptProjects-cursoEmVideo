7e0cfa5623f829b411921d8f147ffcdb44ed9ee7
Eduardo Escaleira <duescaleira@gmail.com>
2024-01-22 10:15:50 -0300

    aula finalizada

--- aula11 - Condições/ex008.js
+++ aula11 - Condições/ex008.js
@@ -1,10 +1,16 @@
-var vel = 55
-console.log(`A velocidade do seu carro é de ${vel} km/h`)
+// var vel = 55
+// console.log(`A velocidade do seu carro é de ${vel} km/h`)
 
-if (vel > 60) {
-    console.log(`MULTADO! Velocidade acima da permitida`)    
+// if (vel > 60) {
+//     console.log(`MULTADO! Velocidade acima da permitida`)    
+// } else {
+//     console.log(`Dirija sempre usando o cinto de segurança!`)
+// }
+
+var pais = 'Brasil'
+
+if (pais == 'Brasil') {
+    console.log('Brasileiro!')
 } else {
-    console.log(`Dirija sempre usando o cinto de segurança!`)
+    console.log('Estrangeiro!')
 }
-
-
