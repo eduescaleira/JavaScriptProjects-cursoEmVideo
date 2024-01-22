d9d8fc065d947f9d9cee159a2a6837c93e2912ab
Eduardo Escaleira <duescaleira@gmail.com>
2024-01-22 09:42:37 -0300

    condicoes

---
+++ aula11 - Condições/ex008.js
@@ -0,0 +1,10 @@
+var vel = 55
+console.log(`A velocidade do seu carro é de ${vel} km/h`)
+
+if (vel > 60) {
+    console.log(`MULTADO! Velocidade acima da permitida`)    
+} else {
+    console.log(`Dirija sempre usando o cinto de segurança!`)
+}
+
+
