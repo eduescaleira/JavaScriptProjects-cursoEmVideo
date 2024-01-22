3a1863c0660d759987eb08a5d0c8a89423cc2fbf
Eduardo Escaleira <duescaleira@gmail.com>
2024-01-22 12:01:13 -0300

    estrutura básica do site finalizada

--- aula12 - Condições II/ex011.js
+++ aula12 - Condições II/ex011.js
@@ -1,10 +1,11 @@
 var idade = 67
+console.log(`Você tem ${idade} anos.`)
 
 if (idade < 16) {
-    console.log("Não vota")
+    console.log("Você não pode votar.")
 } else if (idade < 18 || idade > 65) {
-    console.log("Voto opcional") 
+    console.log("Seu voto é opcional") 
 } else {
-    console.log("Voto obrigatório")
+    console.log("Seu voto é obrigatório")
 }
 
