e2dffd71944b7a60040230c816856cb6035cd101
Eduardo Escaleira <duescaleira@gmail.com>
2024-01-23 10:45:13 -0300

    aula FOR finalizada

--- aula13 - Repetições - WHILE/ambiente.js
+++ aula13 - Repetições - WHILE/ambiente.js
@@ -1,3 +1,13 @@
 // console.log("Tudo bem?")
 
-var n = 0
+var n = 1
+
+// while (n < 8) {
+//     console.log(`Passo ${n}`)
+//     n ++
+// }
+
+do {
+    console.log(`Passo ${n}`)
+    n++
+} while (n <= 2)
