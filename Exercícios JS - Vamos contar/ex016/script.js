70a8b99caab431ad4ee96d2de3edf93200a8778d
Eduardo Escaleira <duescaleira@gmail.com>
2024-01-23 12:56:19 -0300

    exercicio finalizado

--- Exercícios JS - Vamos contar/ex016/script.js
+++ Exercícios JS - Vamos contar/ex016/script.js
@@ -1,12 +1,30 @@
 function contar() {
-    var inicio = parseInt(window.document.getElementById('txtinc').value);
-    var fim = parseInt(window.document.getElementById('txtfim').value);
-    var contador = parseInt(window.document.getElementById('txtcon').value);
+    var inicio = window.document.getElementById('txtinc');
+    var fim = window.document.getElementById('txtfim');
+    var contador = window.document.getElementById('txtcon');
     var res = window.document.getElementById('res');
 
     res.innerHTML = ''; // Limpa o conteúdo anterior
 
-    for (var i = inicio; i <= fim; i += contador) {
-        res.innerHTML += i + '<br>';
+    if (inicio.value.length == 0 || fim.value.length == 0 || contador.value.length == 0) {
+        window.alert("[ERRO] Faltam dados")
+        res.innerHTML = 'Impossível contar'
+    } else {
+        res.innerHTML = "Contando: "
+        let i = Number(inicio.value)
+        let f = Number(fim.value)
+        let p = Number(contador.value)
+
+        if (i < f) {
+        for (let c = i; c <= f; c += p) {
+            res.innerHTML += `${c} \u{1F449}`
+            }
+    
+        } else {
+            for (let c = i; c >= f; c -= p) {
+                res.innerHTML += `${c} \u{1F449}`
+                }
+            res.innerHTML += `\u{1F3C1}`
+        }
     }
 }
